2d75fa0db3a012725fe313d7a30ecd148847a0ed
Cherry Ramatis <cherry.ramatis@icloud.com>
2024-05-22 15:32:25 -0300

    FINALLY LOADED THE MODULE

--- elixir/lib/compile.ex
+++ elixir/lib/compile.ex
@@ -1,7 +1,25 @@
 defmodule BeamCherry.Compile do
+  @tags %{
+    u: 0,
+    i: 1,
+    a: 2,
+    x: 3,
+    f: 5
+  }
+
+  @opcodes %{label: 1, func_info: 2, return: 19, int_code_end: 3}
+
+  import Bitwise
+
   @spec call(ast :: list(BeamCherry.Tokenizer.t())) :: {:ok} | {:err}
   def call(_ast) do
-    beam_content = ["BEAM"] ++ code_chunk() ++ atom_chunk(["output", "hello", "world"])
+    beam_content =
+      ["BEAM"] ++
+        chunk(:code) ++
+        chunk(:import) ++
+        chunk(:export) ++
+        chunk(:str) ++
+        chunk(:atom, ["output"])
 
     bytes =
       ["FOR1", arr_byte_length(beam_content) |> to_be_bytes()] ++
@@ -15,37 +33,65 @@ defmodule BeamCherry.Compile do
     {:ok}
   end
 
-  @spec code_chunk() :: [binary()]
-  def code_chunk() do
+  defp chunk(:code) do
     instruction_set = 0
     sub_size = 16
     opcode_max = 169
-    label_count = 0
-    function_count = 0
+    label_count = 3
+    function_count = 1
 
-    chunk = [
-      to_be_bytes(sub_size),
-      to_be_bytes(instruction_set),
-      to_be_bytes(opcode_max),
-      to_be_bytes(label_count),
-      to_be_bytes(function_count)
-    ]
+    chunk =
+      [
+        to_be_bytes(sub_size),
+        to_be_bytes(instruction_set),
+        to_be_bytes(opcode_max),
+        to_be_bytes(label_count),
+        to_be_bytes(function_count),
+        <<@opcodes[:label]>>
+      ] ++
+        encode(@tags[:u], 1) ++
+        [<<@opcodes[:func_info]>>] ++
+        encode(@tags[:a], 1) ++
+        encode(@tags[:a], 1) ++
+        encode(@tags[:u], 0) ++
+        [<<@opcodes[:label]>>] ++
+        encode(@tags[:u], 2) ++
+        [<<@opcodes[:return]>>] ++
+        [<<@opcodes[:int_code_end]>>]
 
     ["Code", arr_byte_length(chunk) |> to_be_bytes()] ++ pad_chunk(chunk)
   end
 
-  @spec atom_chunk([String.t()]) :: [binary()]
-  def atom_chunk(atoms) do
+  defp chunk(:import) do
+    import_count = 0
+
+    chunk = [to_be_bytes(import_count)]
+
+    ["ImpT", arr_byte_length(chunk) |> to_be_bytes()] ++ pad_chunk(chunk)
+  end
+
+  defp chunk(:export) do
+    export_count = 0
+
+    chunk = [to_be_bytes(export_count)]
+
+    ["ExpT", arr_byte_length(chunk) |> to_be_bytes()] ++ pad_chunk(chunk)
+  end
+
+  defp chunk(:str) do
+    ["StrT", to_be_bytes(0)]
+  end
+
+  defp chunk(:atom, atoms) do
     chunk =
       [length(atoms) |> to_be_bytes()] ++
-        Enum.map(atoms, fn atom -> byte_size(atom) |> to_be_bytes() end) ++ atoms
+        Enum.map(atoms, fn x -> <<byte_size(x)>> end) ++ atoms
 
     ["AtU8", arr_byte_length(chunk) |> to_be_bytes()] ++ pad_chunk(chunk)
   end
 
-  def pad_chunk(chunk) do
-    total_bytes = Enum.reduce(chunk, 0, fn x, acc -> acc + byte_size(x) end)
-    chunk_rem = rem(total_bytes, 4)
+  defp pad_chunk(chunk) do
+    chunk_rem = rem(arr_byte_length(chunk), 4)
 
     if chunk_rem != 0 do
       chunk ++ [:binary.copy(<<0>>, 4 - chunk_rem)]
@@ -58,5 +104,15 @@ defmodule BeamCherry.Compile do
   defp arr_byte_length(chunks), do: chunks |> Enum.join() |> byte_size()
 
   @spec to_be_bytes(integer()) :: binary()
-  def to_be_bytes(num) when is_integer(num), do: <<num::size(32)-big-integer>>
+  defp to_be_bytes(num) when is_integer(num), do: <<num::size(32)-big-integer>>
+
+  @spec encode(integer(), integer()) :: [binary()]
+
+  # TODO: Negative numbers
+  defp encode(_, n) when n < 0, do: []
+
+  defp encode(tag, n) when n < 16, do: [<<n <<< 4 ||| tag>>]
+
+  # TODO: Large numbers
+  defp encode(_, _), do: []
 end
